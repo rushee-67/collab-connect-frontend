e1b1b634da8ed77094edba7c0a0acd5eef20f97e
Rusheenddra Basani <rusheenddrabasani@gmail.com>
2025-10-18 12:37:13 +0530

    Use environment variable for backend URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // backend URL
+  baseURL: `${import.meta.env.VITE_SERVER_URL}/api` || "http://localhost:5000/api",
 });
 
 // Attach token automatically
